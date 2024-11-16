c4ef059c9eb2d1b399d59f074119dde9ebc9d22b
websitenow <icarorubem1@gmail.com>
2024-11-16 16:57:34 -0300

    Update Upload

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-  return "HomePage"
+  return render_template("homepage.html")
+
+@app.errorhandler(Exception)
+def error(e):
+  return f"Error: {e}"
 
 if __name__ == '__main__':
-  app.run()
+  app.run(debug=True)
