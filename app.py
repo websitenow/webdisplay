c8f58003c9a12bdd6b1104f35c6c6e3e6addea7d
Developer <123030444+websitenow@users.noreply.github.com>
2024-11-21 12:15:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -83,36 +83,36 @@ if __name__ == '__main__':
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
-class SocketIO_(SocketIO):
-    def __call__(
-        self, environ: WSGIEnvironment, start_response: StartResponse
-    ) -> cabc.Iterable[bytes]:
-        """The WSGI server calls the Flask application object as the
-        WSGI application. This calls :meth:`wsgi_app`, which can be
-        wrapped to apply middleware.
-        """
-        print(environ, start_response)
+class SocketIOFlask:
+    def __init__(self, name):
+        self.app = Flask(name)
+        self.socketIO = SocketIO(self.app, async_mode='eventlet')
+        
+    def __call__(self, environ, start_response): 
+        return self.app.__call__(environ, start_response)
+    
+    def run(self, *a, **kw):
+        self.socketIO.run(self.app, *a, **kw)
+    
+app = SocketIOFlask(__name__)
 
-app_server = Flask(__name__)
-app = SocketIO_(app_server, async_mode='eventlet')  # Usar eventlet
-
-@app_server.route('/')
+@app.app.route('/')
 def home():
     return render_template('homepage.html')
 
-@app_server.route('/receiver')
+@app.app.route('/receiver')
 def receiver():
     return render_template('receiver.html')
 
-@app.on('offer')
+@app.socketIO.on('offer')
 def handle_offer(data):
     emit('offer', data, broadcast=True)
 
-@app.on('answer')
+@app.socketIO.on('answer')
 def handle_answer(data):
     emit('answer', data, broadcast=True)
 
-@app.on('candidate')
+@app.socketIO.on('candidate')
 def handle_candidate(data):
     emit('candidate', data, broadcast=True)
 
